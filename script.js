c17f8a3c33aeb5fdd68ad4aad80297a392c91f2e
hboobstard <johnjohnsonn754@gmail.com>
2026-08-11 07:50:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.querySelectorAll(".copy-button").forEach(function (button) {
+    button.addEventListener("click", function () {
 
+        const address = button
+            .closest(".wallet-content")
+            .querySelector(".wallet-address")
+            .textContent
+            .trim();
+
+        navigator.clipboard.writeText(address);
+
+        const originalText = button.textContent;
+
+        button.textContent = "COPIED!";
+
+        setTimeout(function () {
+            button.textContent = originalText;
+        }, 1500);
+    });
+});
